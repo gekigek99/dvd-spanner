99fee2d338f3a8e0f2e921b2496b5428c764f3d8
gekigek99 <53654579+gekigek99@users.noreply.github.com>
2022-04-09 13:15:53 +0200

    changed example folder structure

--- dvd-file-assembler.py
+++ dvd-file-assembler.py
@@ -1,13 +1,13 @@
 import os
 
-dataToDvds = ".\exampledata"	# folder where data is stored and must be splitted on multiple dvds
-par2ToDvds = ".\examplepar2"	# folder where par2 data is stored and must be splitted on multiple dvds
-inAllDvds =   ".\inall"			# folder where data must be present on all dvds
+dataFolder = ".\\example\\data\\"		# folder where data is stored and must be splitted on multiple dvds
+par2Folder = ".\\example\\par2\\"		# folder where par2 data is stored and must be splitted on multiple dvds
+duplFolder  = ".\\example\\duplicate\\"	# folder where data must be present on all dvds
 
 dvdDimension = 100000000
 
 def main():	
-	for (root, dirs, file) in os.walk(dataToDvds):
+	for (root, dirs, file) in os.walk(dataFolder):
 		print(root)
 		print(dirs)
 		print(file, end="\n\n")
